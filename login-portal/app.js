2d9dd638999fde8c9f87527aba0eadd362b745fd
Nicola Folchi <nico93@live.unc.edu>
2019-10-12 17:24:56 -0400

    Uploading files generated by express.

---
+++ login-portal/app.js
@@ -0,0 +1,47 @@
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let logger = require('morgan');
+let session = require('express-session');
+
+
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: "esta es mi string para que mis cookies se mantengan seguras, deliciosas y bueno, crugientes jojo",
+  resave: true,
+  saveUninitialized: false
+}));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
