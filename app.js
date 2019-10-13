f85ffa0249e3b0cacb52188d10b42f8ef044f54e
Nicola Folchi <nico93@live.unc.edu>
2019-10-12 22:48:25 -0400

    able to create an account. Having problems displaying dashboard

--- app.js
+++ app.js
@@ -4,12 +4,35 @@ let path = require('path');
 let logger = require('morgan');
 let session = require('express-session');
 
+// ========= routes ==============
+let dashboardRouter = require("./routes/dashboard");
+let publicRouter = require("./routes/public");
+let usersRouter = require("./routes/users");
+
 //okta API
 let okta = require("@okta/okta-sdk-nodejs");
 let ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
-
-let dashboardRouter = require("./routes/dashboard");         
-let publicRouter = require("./routes/public");
+//user authentication with okta
+let oktaClient = new okta.Client({
+  orgUrl: '{https://dev-838812.okta.com}',
+  token: "00dtUt2yZHG9W0CZ4sMF1_8M-N87o7wXQhWCLJBpux"
+});
+const oidc = new ExpressOIDC({
+  issuer: "https://dev-838812.okta.com/oauth2/default",
+  client_id: "0oa1kkvwcp9TYd7nu357",
+  client_secret: "cjrFAlzTnWNrfQ73ANS4T7znss3nVzP6ulTPF1jH",
+  redirect_uri: 'http://localhost:3000/users/callback',
+  scope: "openid profile",
+  routes: {
+    login: {
+      path: "/users/login"
+    },
+    callback: {
+      path: "/users/callback",
+      defaultRedirect: "/dashboard"
+    }
+  }
+});
 
 let app = express();
 
@@ -27,9 +50,36 @@ app.use(session({
   resave: true,
   saveUninitialized: false
 }));
+app.use(oidc.router);
 
+// this allows us to access the user information stored at okta at any time.
+app.use(function(req, res, next) {
+  if (!req.userinfo) {
+    return next();
+  }
+
+  oktaClient.getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      res.locals.user = user;
+      next();
+    }).catch(err => {
+      next(err);
+    });
+});
+//middleware that prevents an non-user to visit a given route if they are not an user
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    return res.status(401).render("unauthenticated");
+  }
+
+  next();
+}
+
+// enabling of routes
 app.use('/', publicRouter);
-app.use('/dashboard', dashboardRouter);
+app.use('/dashboard', loginRequired, dashboardRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+let express = require("express");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+let router = express.Router();
+
+// Log a user out
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
 });
+
 
 module.exports = router;
